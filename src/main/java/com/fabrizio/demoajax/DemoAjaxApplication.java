5284e15a7b5a4a98a0ae468ca1f2821bc406c724
Fabrizio Abreu <fabrizioabreu@live.com>
2021-04-07 17:15:15 -0300

    Capturando conteudo via OpenGraph e testando no console

--- src/main/java/com/fabrizio/demoajax/DemoAjaxApplication.java
+++ src/main/java/com/fabrizio/demoajax/DemoAjaxApplication.java
@@ -1,13 +1,28 @@
 package com.fabrizio.demoajax;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fabrizio.demoajax.domain.SocialMetaTag;
+import com.fabrizio.demoajax.service.SocialMetaTagService;
+
 @SpringBootApplication
-public class DemoAjaxApplication {
+public class DemoAjaxApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAjaxApplication.class, args);
+	}
+
+	@Autowired
+	SocialMetaTagService service;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		SocialMetaTag og = service.getOpenGraphByUrl("https://github.com/fabrizioabreu");
+		System.out.println(og.toString());
 	}
 
 }
---
+++ src/main/java/com/fabrizio/demoajax/service/SocialMetaTagService.java
@@ -0,0 +1,28 @@
+package com.fabrizio.demoajax.service;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Service;
+
+import com.fabrizio.demoajax.domain.SocialMetaTag;
+
+@Service
+public class SocialMetaTagService {
+
+	public SocialMetaTag getOpenGraphByUrl(String url) {
+		SocialMetaTag tag = new SocialMetaTag();
+		// armazenando obj recuperado pelo jsoup
+		try {
+			Document doc = Jsoup.connect(url).get();
+			tag.setTitle(doc.head().select("meta[property=og:title]").attr("content"));
+			tag.setSite(doc.head().select("meta[property=og:site_name]").attr("content"));
+			tag.setImage(doc.head().select("meta[property=og:image]").attr("content"));
+			tag.setUrl(doc.head().select("meta[property=og:url]").attr("content"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return tag;
+	}
+}
