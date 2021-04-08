dcff11733a870426d7ebebfc9b5ffbfb5babd34b
Fabrizio Abreu <fabrizioabreu@live.com>
2021-04-08 16:43:52 -0300

    Criando função em ajax para capturar informações via URL

---
+++ src/main/resources/static/js/promo-add.js
@@ -0,0 +1,18 @@
+// Funcao para capturar as metatags
+$("#linkPromocao").on('change', function(){
+	//Capturnado URL do campo de imput
+	var url = $(this).val();
+	
+	// Verificando se tem uma url no campo de imput 'http://'
+	if (url.length > 7) {
+		// Incluindo método de requisição baseado em AJAX
+		$.ajax({
+			method:"POST",
+			url: "/meta/info?url=" + url, // Caminho do SocialMetaTagController
+			cache: false, // Não iremos fazer uso de cache
+			success: function( data ) {	
+				console.log(data);	// Passamos uma função que vai receber o resultado da operação
+			}
+		});
+	}
+});
