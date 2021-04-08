59240c5682ecdc3a7961a0ff25bf5da80e9bde15
Fabrizio Abreu <fabrizioabreu@live.com>
2021-04-08 18:42:13 -0300

    Limpando os dados da solicitação anterior

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -10,13 +10,19 @@ $("#linkPromocao").on('change', function() {
 			method: "POST",
 			url: "/meta/info?url=" + url, // Caminho do SocialMetaTagController
 			cache: false, // Não iremos fazer uso de cache
+			beforeSend: function() {	// Limpando dados do formulário
+				$("#alert").removeClass("alert alert-danger").text("");
+				$("#titulo").val("");
+				$("#site").text("");
+				$("#linkImagem").attr("src", "/images/promo-dark.png");
+			},
 			success: function(data) {
 				console.log(data);	// Passamos uma função que vai receber o resultado da operação
 
 				// Acessando o componente HTML que vai receber as informações
 				$("#titulo").val(data.title);
-				$('#site').text(data.site.replace("@", ""));
-				$('#linkImagem').attr("src", data.image);
+				$("#site").text(data.site.replace("@", ""));
+				$("#linkImagem").attr("src", data.image);
 			},
 			// Função para ERRO 404
 			statusCode: {
