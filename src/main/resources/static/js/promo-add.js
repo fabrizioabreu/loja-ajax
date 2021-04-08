87ea805cce6eee2b0fa6396107a6af6e92bca4db
Fabrizio Abreu <fabrizioabreu@live.com>
2021-04-08 17:18:03 -0300

    Capturando erro 404 e 500 e mostrando ao usuario

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -1,23 +1,34 @@
 // Funcao para capturar as metatags
-$("#linkPromocao").on('change', function(){
+$("#linkPromocao").on('change', function() {
 	//Capturnado URL do campo de imput
 	var url = $(this).val();
-	
+
 	// Verificando se tem uma url no campo de imput 'http://'
 	if (url.length > 7) {
 		// Incluindo método de requisição baseado em AJAX
 		$.ajax({
-			method:"POST",
+			method: "POST",
 			url: "/meta/info?url=" + url, // Caminho do SocialMetaTagController
 			cache: false, // Não iremos fazer uso de cache
-			success: function( data ) {	
+			success: function(data) {
 				console.log(data);	// Passamos uma função que vai receber o resultado da operação
-				
+
 				// Acessando o componente HTML que vai receber as informações
 				$("#titulo").val(data.title);
 				$('#site').text(data.site.replace("@", ""));
 				$('#linkImagem').attr("src", data.image);
+			},
+			// Função para ERRO 404
+			statusCode: {
+				404: function() {
+					// Adicionando Classe de alerta do Bootstrap
+					$("#alert").addClass("alert alert-danger").text("Nenhuma informação pode ser recuperada dessa url");
+				}
+			},
+			error: function() {
+				$("#alert").addClass("alert alert-danger").text("Ops... algo deu errado, tente mais tarde.");
 			}
+
 		});
 	}
 });
