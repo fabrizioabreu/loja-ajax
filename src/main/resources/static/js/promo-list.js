2eca198acba762aee5024f4e923c01087c9b2903
Fabrizio Abreu <fabrizioabreu@live.com>
2021-04-12 18:14:05 -0300

    Exibindo novos intens com o infinte Scroll

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -2,6 +2,12 @@ var pageNumber = 0;
 
 // EFEITO INFINIT SCROLL
 
+// Rotina de quando abrirmos a página pela primeira vez.
+$(document).ready(function(){
+	$("#loader-img").hide();	// Escondendo o load
+	$("#fim-btn").hide();	// Escondendo o load
+});
+
 // Sabendo quando estamos rolando com o scroll do mouse
 $(window).scroll(function() {
 	
@@ -28,8 +34,25 @@ function loadByScrollBar(pageNumber) {
 		data: {
 			page: pageNumber 
 		},
+		beforeSend: function() {
+			$("#loader-img").show();	// Quando a função iniciar, mostramos a imagem de load.
+		},
 		success: function( response ){
-			console.log("resposta > ", response);
+			// console.log("resposta > ", response);
+			
+			if(response.length > 150) {
+				$('.row').append( $(response).hide().fadeIn(400) );
+			}
+			else{
+				$("#fim-btn").show();	// Mostra o botão
+				$("#loader-img").removeClass("loader");	// Removemos o load.
+			}
+		},
+		error: function(xhr) {
+			alert("Ops, ocorrou um erro: " + xhr.status + " - " + xhr.statusText);
+		},
+		complete: function() {
+			$("#loader-img").hide();	// Ao completar, escondemos a imagem de load.
 		}
 	})
 }
