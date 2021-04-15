b803aad1cf3b7a14791bb5733e65c5d9f6f072b2
Fabrizio Abreu <fabrizioabreu@live.com>
2021-04-15 18:45:11 -0300

    Trabalhando com DATATABLES - Capturando ID da linha e Abrindo os modais

--- src/main/resources/static/js/promo-datatables.js
+++ src/main/resources/static/js/promo-datatables.js
@@ -1,33 +1,103 @@
 // Código dentro do "$(document).ready" será executado pelo jquery logo depois que a página HTML for aberta
-$(document).ready(function(){
+$(document).ready(function() {
 	moment.locale('pt-br');
-	$("#table-server").DataTable({
-		
+	var table = $("#table-server").DataTable({
+
 		processing: true,	// Informa ao usuário que os dados estão sendo carregados.
 		serverSide: true,	// habilitar DataTable para trabalharmos do lado do servidor.
 		responsive: true,	// Faz com que a tabela tenha o comportamento responsivo.
-		lengthMenu: [ 10, 15, 20, 25 ],	// Tamanho do menu de páginas
-		
+		lengthMenu: [10, 15, 20, 25],	// Tamanho do menu de páginas
+
 		ajax: {
 			url: "/promocao/datatables/server",
 			data: "data"
 		},
 		// Informação referente as colunas, cada linha da tabela
 		columns: [
-			{data: 'id'},
+			{ data: 'id' },
 			{data: 'titulo'},
-			{data: 'site'},
-			{data: 'linkPromocao'},
-			{data: 'descricao'},
-			{data: 'linkImagem'},
-			{data: 'preco', render: $.fn.dataTable.render.number('.', ',', 2, 'R$ ')},	// Formatando para padrão brasileiro
-			{data: 'likes'},
-			{data: 'dtCadastro', render: 	// Formatando DATA padrão Brasil
+			{ data: 'site' },
+			{ data: 'linkPromocao' },
+			{ data: 'descricao' },
+			{ data: 'linkImagem' },
+			{ data: 'preco', render: $.fn.dataTable.render.number('.', ',', 2, 'R$ ') },	// Formatando para padrão brasileiro
+			{ data: 'likes' },
+			{ data: 'dtCadastro', render: 	// Formatando DATA padrão Brasil
 					function(dtCadastro) {
-						return moment( dtCadastro ).format('LLL');
+						return moment(dtCadastro).format('LLL');
 					}
-			},	
-			{data: 'categoria.titulo'}
+			},
+			{ data: 'categoria.titulo' }
+		],
+
+	// ====================================== INCLUINDO BOTÕES EDITER e EXCLUIR =====================================
+
+		dom: 'Bfrtip',
+		buttons: [
+			{
+				text: 'Editar',
+				attr: {
+					id: 'btn-editar',
+					type: 'button'
+				},
+				enabled: false	// Desabilitar o botão assim que a página for aberta
+			},
+			{
+				text: 'Excluir',
+				attr: {
+					id: 'btn-excluir',
+					type: 'button'
+				},
+				enabled: false	// Desabilitar o botão assim que a página for aberta
+			}
 		]
 	});
+	
+	// ============================ Ação para MARCAR/DESMARCAR botões ao clicarna ordenação =================================
+	
+	$("#table-server thead").on('click', 'tr', function() {
+		table.buttons().disable();
+	});
+	
+	// ====================================== Ação para MARCAR/DESMARCAR linhas clicadas =====================================
+	
+	$("#table-server tbody").on('click', 'tr', function() {
+		if ($(this).hasClass('selected')) {		// Verificar se existe a classe 'selected'
+			$(this).removeClass('selected');
+			table.buttons().disable();
+		} else {
+			$('tr.selected').removeClass('selected');	// Removendo todas outras linhas selecionadas
+			$(this).addClass('selected');
+			table.buttons().enable();
+		}
+	});
+
+	// Capturando o click
+	$("#btn-editar").on('click', function() {
+		if (isSelectedRow()) {
+			$("#modal-form").modal('show');
+		}
+	});
+
+	$("#btn-excluir").on('click', function() {
+		if (isSelectedRow()) {	
+			$("#modal-delete").modal('show');
+		}
+		
+	});
+
+	function getPromoId() {
+		return table.row(table.$('tr.selected')).data().id;	// Recuperando o ID
+	}
+	
+	function isSelectedRow() {
+		var trow = table.row(table.$('tr.selected'));
+		return trow.data() !== undefined
+	}
+
 });
+
+
+
+
+
